ab1ed5ac9ebf8b7bf380c0695a64eb5ad03c5de2
indigo-lab <indigo-lab@users.noreply.github.com>
2016-04-05 13:26:19 +0900

    Add license header

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,20 @@ rm -rf out || exit 0;
 mkdir out;
 
 npm install
-node main.js > out/geonamesjp_vs_yan.jsonld
+
+cat << EOS > out/geonamesjp_vs_yan.nt.txt
+# Copyright (c) 2016 Indigo Corp. Research and Development Center
+#
+# このデータはクリエイティブ・コモンズ 表示 - 継承 4.0 国際 ライセンスの下に提供されています。
+# <https://creativecommons.org/licenses/by-sa/4.0/deed.ja>
+#
+# このデータは Yokohama Art LOD SPARQLエンドポイント の出力を加工して作成されました。
+# <http://data.yafjp.org/SPARQL.html>
+#
+
+EOS
+
+node main.js >> out/geonamesjp_vs_yan.nt.txt
 
 # go to the out directory and create a *new* Git repo
 cd out
@@ -20,4 +33,3 @@ git commit -m "Deploy to GitHub Pages"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 
 # git push "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
-
